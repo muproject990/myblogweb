505c7fd8b2b27d04e58aa68397f9c51a730f6cc9
Aavash Paudel <muproject990@gmail.com>
2024-07-18 10:33:32 +0545

    basic layout implementd

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,22 +1,3 @@
-
-<!doctype html>
-            <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts</title>
-    {{date('Y-m-d')}}
-</head>
-<body>
-<h1>Post index page</h1>
-
-{{$AGE}}
-{{$name}}
-
-@foreach($posts as $post)
-    <li>{{$post}}</li>
-@endforeach
-</body>
-</html>
+<x-layout>
+    <h1>index</h1>
+</x-layout>
