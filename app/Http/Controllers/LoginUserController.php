18c2cf5a20916bef3c7ef568575d1e0f5c092b90
Aavash Paudel <muproject990@gmail.com>
2024-07-20 10:48:53 +0545

    only authorized user can edit or delete post using conditional check

---
+++ app/Http/Controllers/LoginUserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginUserController extends Controller
+{
+
+    public function login(Request $request){
+        return view('auth.login');
+    }
+
+
+
+        public function store(Request $request){
+        $request->validate([
+           'email'=>'required|email',
+           'password'=>'required|min:6'
+        ]);
+
+//        Attempt to logi in user
+
+            if (Auth::guard('web')->attempt(
+                [
+                    'email'=>$request->email,
+                    'password'=>$request->password
+        ])){
+                return redirect()->intended(route('posts.index'));
+            }
+            else{
+                return  back()->withErrors([
+                    'email' => 'The provided credentials do not match our records.',
+                ]);
+            }
+    }
+
+
+
+    public  function  logout(Request $request)
+    {
+                Auth::guard('web')->logout();
+
+                $request->session()->invalidate();
+                $request->session()->regenerateToken();
+                return to_route('posts.create');
+
+
+    }
+
+
+}
---
+++ app/Http/Controllers/RegisterUserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
+
+class RegisterUserController extends Controller
+{
+    public function register(Request $request){
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' =>[ 'required','max:255','min:5','string'],
+            'email' =>'required|min:8|unique:users|email',
+            'password' => 'required|min:8|confirmed',
+
+        ]);
+
+    $user=User::create(
+        [
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make( $request->password)
+        ]
+
+    );
+
+    auth()->login($user);
+    return to_route('posts.index');
+
+
+    }
+}
